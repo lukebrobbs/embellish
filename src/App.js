d6b47c54690fe3d0ab32bbc0c902e19ef2546209
Luke <lukemichaelvocalist@gmail.com>
2018-05-01 12:28:32 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import Home from "./components/Home";
+import logo from "./logo.svg";
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return <Home />;
+  }
+}
+
+export default App;
